693c9521abc57eb3542fdb54898c8e4beb067a9b
cristian <cristiancamilocruzosorio5@gmail.com>
2026-09-23 12:40:07 -1000

    agrege el ciclo while y con los respectivos si y sino y su false y true

--- entregable_momento1.js
+++ entregable_momento1.js
@@ -6,4 +6,26 @@ function validarAcceso() {
     const MAX_INTENTOS = 3;
     let intentos = 0;
     let acceso = false;
+
+        while (intentos < MAX_INTENTOS && !acceso) {
+        let usuarioIngresado = prompt("Ingrese su usuario:");
+        let contrasenaIngresada = prompt("Ingresa tu contraseña:");
+
+        
+        if (usuarioIngresado === USUARIO_CORRECTO && contrasenaIngresada === CONTRASENA_CORRECTA) {
+            acceso = true;
+            console.log("¡Bienvenido chico/a al sistema siii!");
+        } else {
+
+            intentos++;
+            
+            if (intentos < MAX_INTENTOS) {
+                console.log("Datos incorrectos. Intento " + intentos + " de " + MAX_INTENTOS + ".");
+            } else {
+                console.log("Usuario bloqueado Ha superado el número de intentos");
+            }
+        }
+    }
 }
+
+validarAcceso();
